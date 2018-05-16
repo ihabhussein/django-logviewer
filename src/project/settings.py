140138ccd79a83ce73676f968f49be03440ea874
Ihab Hussein <ihab@ihabhussein.com>
2018-05-16 12:27:52 +0200

    Use dropdown lists for filtering

--- src/project/settings.py
+++ src/project/settings.py
@@ -12,13 +12,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'rsyslog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rsyslog',
 ]
 
 MIDDLEWARE = [
