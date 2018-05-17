336d505ffebc54bb3766c05c369e8ebb271f0910
Ihab Hussein <ihab@ihabhussein.com>
2018-05-17 13:34:48 +0200

    Clean up the models a bit

--- src/rsyslog/models.py
+++ src/rsyslog/models.py
@@ -19,14 +19,13 @@ class Priority(models.Model):
 
 
 class Event(models.Model):
-    id = models.IntegerField(_('ID'), primary_key=True)
     time_reported = models.DateTimeField(_('Time Reported'), null=True)
     time_received = models.DateTimeField(_('Time Received'), null=True, db_index=True)
-    host = models.CharField(_('From Host'), max_length=32, null=True)
+    host = models.CharField(_('Host'), max_length=32, null=True)
     facility = models.ForeignKey(Facility, on_delete=models.SET_NULL, null=True, db_index=True)
     priority = models.ForeignKey(Priority, on_delete=models.SET_NULL, null=True, db_index=True)
     message = models.TextField(_('Message'), null=True)
-    tag = models.CharField(_('Syslog Tag'), max_length=32, null=True, db_index=True)
+    tag = models.CharField(_('Program'), max_length=32, null=True, db_index=True)
 
     @classmethod
     def tags(self):
